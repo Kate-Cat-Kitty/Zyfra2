4107e646a8b030b3a74fb54316cfaaffeb2db2d0
Kate-Cat-Kitty <gundina986@gmail.com>
2024-12-05 10:19:54 +0300

    added context

---
+++ lab2/Migrations/20241205063215_FixContext.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Sessions",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Sessions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "Sessions",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Sessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Sessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Sessions",
+                table: "Sessions",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_UserId",
+                table: "Sessions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sessions_Users_UserId",
+                table: "Sessions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sessions_Users_UserId",
+                table: "Sessions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Sessions",
+                table: "Sessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sessions_UserId",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Sessions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionId",
+                table: "Sessions",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(36)",
+                oldMaxLength: 36);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Sessions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Sessions",
+                table: "Sessions",
+                column: "SessionId");
+        }
+    }
+}
